efae7aaac953c97042e44c24524baf92bc641886
johnstonjacob <jacob@johnstonjacob.com>
2018-10-15 19:05:16 -0500

    fix for multiple instances

--- update_deploy.py
+++ update_deploy.py
@@ -31,7 +31,7 @@ def getTaggedInstances(client, value):
             'Name': 'tag:aws:cloudformation:stack-name',
             'Values': [value]
         }])
-    return [instance['InstanceId'] instance for [instance for instanceGroup in instances['Reservations'][0]] if instance['State']['Code'] == 16]
+    return [instance['InstanceId'] for instance in [instance for instanceGroup in instances['Reservations'][0]] if instance['State']['Code'] == 16]
 
 
 if len(sys.argv) < 4:
